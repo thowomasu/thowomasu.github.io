b1f8b6be812ec2f19c78a329b70d0c77d9e06a45
long.tran@myy.haaga-helia.fi <long.tran@myy.haaga-helia.fi>
2024-11-02 21:54:44 +0200

    revamped pricing + other changes

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ function toggleMenu() {
     const navMenu = document.getElementById("nav-menu");
     navMenu.style.display = navMenu.style.display === "block" ? "none" : "block";
   }
+
+  document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('mouseenter', () => {
+      button.style.boxShadow = '2px 2px 10px rgba(255, 255, 255, 0.5)';
+    });
+    button.addEventListener('mouseleave', () => {
+      button.style.boxShadow = 'none';
+    });
+  });
   
